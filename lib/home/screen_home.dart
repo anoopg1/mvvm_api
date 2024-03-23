9cfb7797e6ed0235461ad182d482762de0a35b66
Anoop G <91954886+anoopg1@users.noreply.github.com>
2024-03-23 19:50:03 +0530

    Api state success

--- lib/home/screen_home.dart
+++ lib/home/screen_home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_project/home/screen_home_viewmodel.dart';
-import 'package:mvvm_project/services/product_services.dart';
 import 'package:stacked/stacked.dart';
 
 class ScreenHome extends StatelessWidget {
@@ -15,29 +14,34 @@ class ScreenHome extends StatelessWidget {
       },
       builder: (context, viewModel, child) {
         return Scaffold(
-            body: ListView.separated(
-                itemBuilder: (context, index) {
-                  return Column(
-                    children: [
-                      Container(
-                        height: 200,
-                        width: 200,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: NetworkImage(
-                                viewModel.plist[index].thumbnail.toString()),
-                          ),
+          body: ListView.separated(
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              viewModel.plist[index].thumbnail.toString()),
                         ),
                       ),
-                      Text(viewModel.plist[index].price.toString()),
-                      Text(viewModel.plist[index].brand.toString()),
-                    ],
-                  );
-                },
-                separatorBuilder: (context, index) => SizedBox(
-                      height: 10,
                     ),
-                itemCount: viewModel.plist.length));
+                    Text(
+                      viewModel.plist[index].price.toString(),
+                    ),
+                    Text(
+                      viewModel.plist[index].brand.toString(),
+                    ),
+                  ],
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                    height: 10,
+                  ),
+              itemCount: viewModel.plist.length),
+        );
       },
     );
   }
